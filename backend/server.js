ca7642765d4fcd073890060a74df5a950c25242c
Aman-Pal287 <palaman9786@gmail.com>
2025-08-26 13:03:58 +0530

    backend done

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ const connectToDB = require("./src/db/db");
 const initSocketServer = require("./src/sockets/socket.server");
 const httpServer = require("http").createServer(app);
 
-
 connectToDB();
 initSocketServer(httpServer);
 
