6623372a7ea8573e2fff1bedec303058cad9a4a2
Aman-Pal287 <palaman9786@gmail.com>
2025-09-04 16:43:53 +0530

    cors orgin

--- backend/src/sockets/socket.server.js
+++ backend/src/sockets/socket.server.js
@@ -9,7 +9,7 @@ const { createMemory, queryMemory } = require("../services/vector.service");
 function initSocketServer(httpServer) {
   const io = new Server(httpServer, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: "https://chatgpt-clone-p4mh.onrender.com",
       allowedHeaders: ["Authorization", "Content-Type"],
       credentials: true,
     },
